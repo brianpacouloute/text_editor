d72fe63f3d7fd4160482c54fa44a0ee8d8b53aa3
Brian Pacouloute <paco9857@mylaurier.ca>
2025-02-02 04:14:55 -0500

    Editing console processing.

--- kilo.c
+++ kilo.c
@@ -1,16 +1,36 @@
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
+#include <ctype.h>
+#include <stdio.h>
+//https://viewsourcecode.org/snaptoken/kilo/02.enteringRawMode.html
+struct termios original_terminal;
+
+void disabledRawMode() {
+    tcsetattr(STDIN_FILENO,TCSAFLUSH,&original_terminal);
+}
 
 void enableRawMode() {
-    struct termios raw; /*storing file descriptor into Termios_p a pointed to struct termios holding terminal settings*/
-    tcgetattr(STDIN_FILENO,&raw); /*sets value to struct*/
-    raw.c_lflag &= ~(ECHO); /*This flag dictates if you can see your input in the temrinal*/
+    /*First we store the original terminal settings in a variable to later restore.*/
+    tcgetattr(STDIN_FILENO,&original_terminal); /*sets value to struct*/
+    
+    atexit(disabledRawMode); /*call a funciton when exit*/
+
+    struct termios raw = original_terminal;
+    raw.c_iflag &= ~(ICRNL | IXON);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG); /*This flag dictates if you can see your input in the temrinal*/
     tcsetattr(STDIN_FILENO,TCSAFLUSH,&raw); /*Flush waits for all input to be written and extra input to be flushed before applying changes*/
 }
 
 int main() {
     enableRawMode();
     char c; /*Stored entered character*/
-    while (read(STDIN_FILENO,&c,1) == 1 && c != 'q'); 
+    while (read(STDIN_FILENO,&c,1) == 1 && c != 'q') {
+        if (iscntrl(c)) { /*Checking if char is a control char \n \r \t \b \0*/
+            printf("%d\n",c);
+        } else {
+            printf("%d ('%c')\n",c,c); /*%d gives ASCII code %c gives letter*/
+        }
+    } 
     return 0;
 }
